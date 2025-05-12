cb39470aee9af993d73405c2cfeb3a178f3779f9
henrykwma2024 <henrykwma2024@gmail.com>
2025-05-12 10:14:36 +0800

    Create a T-shirt website

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  protected
+
+  def check_admin_priv
+      if !current_admin
+          redirect_to root_path
+      end
+  end
+end
